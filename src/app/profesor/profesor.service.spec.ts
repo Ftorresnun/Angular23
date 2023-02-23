32f1b65cc771c3be814845ba09e5e888d2cb96be
Ftorresnun <torres.nufed21@zaragoza.salesianos.edu>
2023-02-23 11:47:40 +0100

    cosas

--- src/app/profesor/profesor.service.spec.ts
+++ src/app/profesor/profesor.service.spec.ts
@@ -1,16 +1,16 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
-import { ProfesorService } from './profesor.service';
+import { profesorService } from './profesor.service';
 
 describe('Service: Profesor', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ProfesorService]
+      providers: [profesorService]
     });
   });
 
-  it('should ...', inject([ProfesorService], (service: ProfesorService) => {
+  it('should ...', inject([profesorService], (service: profesorService) => {
     expect(service).toBeTruthy();
   }));
 });
