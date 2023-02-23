01019c56e8e8453ef62d10db929e5e35ab725dd5
Ftorresnun <torres.nufed21@zaragoza.salesianos.edu>
2023-02-23 13:07:25 +0100

    cosa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,18 +10,23 @@ import { AlumnoComponent } from './Alumno/Alumno.component';
 import { AlumnoListaComponent } from './Alumno/alumnoLista/alumnoLista.component';
 import { AlumnoTituloPipe } from './Alumno/alumnoLista/alumnoTitulo.pipe';
 import { AlumnoCountComponent } from './Alumno/alumnoCount/alumnoCount.component';
-import { ProfesorComponent } from './profesor/profesor.component';
+import { profesorComponent } from './profesor/profesor.component';
 
+const appRoutes: Routes = [
+  { path: 'alumnos', component: AlumnoListaComponent },
+  { path: 'profesores', component: profesorComponent },
+
+];
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
     AlumnoComponent,
     AlumnoListaComponent,
     AlumnoTituloPipe,
     AlumnoCountComponent,
-      ProfesorComponent
-   ],
+    profesorComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule,
--- src/app/profesor/Profesor.ts
+++ src/app/profesor/Profesor.ts
@@ -7,7 +7,8 @@ export class profesor implements Iprofesor{
   constructor(
     public nombre: string,
     public apellidos: string,
-    public titulacion :string){
+    public titulacion: string,
+    public fechaIng: Date,){
   }
 
 }
